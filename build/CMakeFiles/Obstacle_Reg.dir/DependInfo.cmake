81e71d9eddf97ade29605453677430bcdb81493c
huy23tran11 <huy23tran11@gmail.com>
2021-07-04 14:59:09 -0700

    build project

---
+++ build/CMakeFiles/Obstacle_Reg.dir/DependInfo.cmake
@@ -0,0 +1,21 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/home/nguyen/Visual-Studio-Workspace/simple_blob_dectector/Obstacle_Reg.cpp" "/home/nguyen/Visual-Studio-Workspace/simple_blob_dectector/build/CMakeFiles/Obstacle_Reg.dir/Obstacle_Reg.cpp.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  "/usr/local/include/opencv4"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ build/CMakeFiles/Obstacle_Reg.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Obstacle_Reg.dir/Obstacle_Reg.cpp.o"
+  "Obstacle_Reg.pdb"
+  "Obstacle_Reg"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/Obstacle_Reg.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
